957fd448cd4de208ec7e3e44bb521d1f801eb53d
Shrey296 <shreyverma296@gmail.com>
2026-02-20 12:50:06 +0530

    Question given in class

--- P8_Main.java
+++ P8_Main.java
@@ -1,4 +1,23 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+class InvalidNameException extends Exception {
+    InvalidNameException(String message) {
+        super(message);
+    }
+}
+
+class InvalidSalaryException extends Exception {
+    InvalidSalaryException(String message) {
+        super(message);
+    }
+}
 
 class Employee {
     String name;
@@ -23,28 +42,48 @@ public class P8_Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String name = "";
+        int age = 0;
+        float salary = 0;
 
-        System.out.print("Enter Name (20 characters max): ");
-        String name = sc.nextLine();
+        try {
+            System.out.print("Enter Name (20 characters max): ");
+            name = sc.nextLine();
 
-        System.out.print("Enter Age (must be more than 5): ");
-        int age = sc.nextInt();
+            if (name.length() > 20) {
+                throw new InvalidNameException("Name can't exceed 20 characters");
+            }
 
-        System.out.print("Enter Salary: ");
-        float salary = sc.nextFloat();
+            System.out.print("Enter Age (must be more than 5): ");
+            age = sc.nextInt();
 
-        if (name.length() <= 20 && age > 5) {
+            if (age <= 5) {
+                throw new InvalidAgeException("Age must be greater than 5");
+            }
+
+            System.out.print("Enter Salary (must be positive): ");
+            salary = sc.nextFloat();
+
+            if (salary < 0) {
+                throw new InvalidSalaryException("Salary cannot be negative");
+            }
 
             Employee e1 = new Employee(name, age, salary);
             e1.display();
-
             System.out.println("\nData is added successfully");
 
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("\nInvalid datatype entered");
+        } 
+        catch (InvalidNameException | InvalidAgeException | InvalidSalaryException e) {
+            System.out.println("\nError: " + e.getMessage());
+        } 
+        catch (Exception e) {
+            System.out.println("\nUnexpected Error occurred");
+        } 
+        finally {
+            sc.close();
         }
-        else {
-            System.out.println("\nInvalid input. Follow the conditions");
-        }
-
-        sc.close();
     }
 }
